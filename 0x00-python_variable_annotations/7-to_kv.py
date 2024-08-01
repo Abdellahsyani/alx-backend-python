bf6e495b120086185d99f40465bb8965183aa5ba
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 19:50:11 +0100

    string numbers tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''working with tuple'''
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    '''return the tuple with sun of int and float'''
+    return k, v
