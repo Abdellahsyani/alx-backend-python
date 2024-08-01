5a5a7a0755710a3e27ba547cb462630995a766cc
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 17:49:59 +0100

    floor.float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''floor annotation'''
+import math
+from typed import Union
+
+
+def floor(n: float) -> Union[int]:
+    '''function that return the floor of the float'''
+    return (math.floor(int))
