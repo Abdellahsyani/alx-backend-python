568c10ae7cd2ea9716c1639a2c557d9801a1f688
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 17:26:35 +0100

    sum of two floating number

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''annotation function presented by abdellah syani'''
 
+
 def add(a: float, b: float) -> float:
-    '''the annotation function that return the sum of two number'''
+    '''the annotation function that
+    return the sum of
+    two floating number'''
     return (a + b)
