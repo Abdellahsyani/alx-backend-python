62f6cf88918e72b1c80532ba3eb66fccd3ba3625
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 17:37:54 +0100

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''concat annotation'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''the function taht return the concatenation of two string'''
+    return (str1 + str2)
