81dc1f9d9b99ae8c31fd57531708f28e2bae12a9
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 17:23:52 +0100

    0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''annotation function presented by abdellah syani'''
 
 def add(a: float, b: float) -> float:
     '''the annotation function that return the sum of two number'''
