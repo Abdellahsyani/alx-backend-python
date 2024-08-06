ce18881cef2471ace543c244621e11a1bd77fbc2
Abdellahsyani <syaniabdos@gmail.com>
2024-08-06 12:53:14 +0100

    4-tasks fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''nodified the wait_n fucntion '''
-task_wait_random = __import__('3-tasks').task_wait_random
 import asyncio
 from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
