9c1625d12df061c74a683d76f3638d9f73a61681
Abdellahsyani <syaniabdos@gmail.com>
2024-08-07 00:23:46 +0100

    writing thr generator by yield

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 '''writing the coroutine'''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''return the list of float number between 0 and 10'''
     for _ in range(10):
         num = random.random() * 10
