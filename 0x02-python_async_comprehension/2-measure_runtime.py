5bd17b7327a22edf25c3fb56a725069da0c99f54
Abdellahsyani <syaniabdos@gmail.com>
2024-08-07 18:04:31 +0100

    2-measure_runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,9 +8,9 @@ async_comprehension = __import__('user').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''return the total time that the parallel take to run'''
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
     start_time = time.time()
-
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-
     return time.time() - start_time
