52eab971bdc865a7d5443da534739073e65bd005
Abdellahsyani <syaniabdos@gmail.com>
2024-08-26 23:15:50 +0100

    add new class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ from utils import access_nested_map
 from parameterized import parameterized
 
 from utils import (
-        access_nested_map.
+        access_nested_map,
         get_json,
         memoize,
 )
